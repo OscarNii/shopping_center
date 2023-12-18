28b9a2d8adccc22d831af37d3615396740835f0d
immanuelsowahoscar@gmail.com <immanuelsowahoscar@gmail.com>
2023-12-18 13:34:42 +0100

    updates on text_theme; Actually working on getting light and dark mode

---
+++ lib/utils/theme/custom_themes/text_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class TTextTheme {
+  TTextTheme._();
+
+  static TextTheme lightThemeTextTheme = const TextTheme();
+
+  static TextTheme darkThemeTextTheme = const TextTheme();
+}
--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -3,19 +3,22 @@ import 'package:flutter/material.dart';
 class TAppTheme {
   TAppTheme._();
   static final ThemeData lightTheme = ThemeData(
-    useMaterial3: true,
-    fontFamily: 'Poppins',
-    brightness: Brightness.light,
-    primaryColor: Colors.blue,
-    // colorScheme: const ColorScheme.light(),
-  );
+      useMaterial3: true,
+      fontFamily: 'Poppins',
+      brightness: Brightness.light,
+      primaryColor: Colors.blue,
+      textTheme: const TextTheme(),
+      scaffoldBackgroundColor: Colors.white);
+
+
+
 
   static final ThemeData darkTheme = ThemeData(
     useMaterial3: true,
     fontFamily: 'Poppins',
     brightness: Brightness.dark,
     primaryColor: Colors.blue,
-    // colorScheme: const ColorScheme.dark(),
+    textTheme: const TextTheme(),
+    scaffoldBackgroundColor: Colors.white,
   );
-    
 }
