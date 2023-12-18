429b05cd41404b0e96788a9630f7a0ab8d1a1c0e
immanuelsowahoscar@gmail.com <immanuelsowahoscar@gmail.com>
2023-12-18 13:37:20 +0100

    Added light and dark text theme to the themes folder

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_center/utils/theme/custom_themes/text_theme.dart';
 
 class TAppTheme {
   TAppTheme._();
@@ -7,18 +8,15 @@ class TAppTheme {
       fontFamily: 'Poppins',
       brightness: Brightness.light,
       primaryColor: Colors.blue,
-      textTheme: const TextTheme(),
+      textTheme: TTextTheme.lightThemeTextTheme,
       scaffoldBackgroundColor: Colors.white);
-
-
-
 
   static final ThemeData darkTheme = ThemeData(
     useMaterial3: true,
     fontFamily: 'Poppins',
     brightness: Brightness.dark,
     primaryColor: Colors.blue,
-    textTheme: const TextTheme(),
+    textTheme: TTextTheme.darkThemeTextTheme,
     scaffoldBackgroundColor: Colors.white,
   );
 }
