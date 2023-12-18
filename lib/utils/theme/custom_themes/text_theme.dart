d029265a24a085dfeae9293522333b7aeebd9764
immanuelsowahoscar@gmail.com <immanuelsowahoscar@gmail.com>
2023-12-18 14:27:49 +0100

    Updated most of the textTheme in the constants

--- lib/utils/theme/custom_themes/text_theme.dart
+++ lib/utils/theme/custom_themes/text_theme.dart
@@ -3,7 +3,73 @@ import 'package:flutter/material.dart';
 class TTextTheme {
   TTextTheme._();
 
-  static TextTheme lightThemeTextTheme = const TextTheme();
+  static TextTheme lightThemeTextTheme = TextTheme(
+    ///headlineLarge
+    headlineLarge: const TextStyle().copyWith(
+        fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.black),
+    headlineMedium: const TextStyle().copyWith(
+        fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.black),
+    headlineSmall: const TextStyle().copyWith(
+        fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.black),
 
-  static TextTheme darkThemeTextTheme = const TextTheme();
+    /// titleLarge
+    titleLarge: const TextStyle().copyWith(
+        fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.black),
+    titleMedium: const TextStyle().copyWith(
+        fontSize: 14.0, fontWeight: FontWeight.bold, color: Colors.black),
+    titleSmall: const TextStyle().copyWith(
+        fontSize: 12.0, fontWeight: FontWeight.bold, color: Colors.black),
+
+    /// bodyLarge
+    bodyLarge: const TextStyle().copyWith(
+        fontSize: 16.0, fontWeight: FontWeight.normal, color: Colors.black),
+    bodyMedium: const TextStyle(
+        fontSize: 14.0, fontWeight: FontWeight.normal, color: Colors.black),
+    bodySmall:  TextStyle(
+        fontSize: 12.0, fontWeight: FontWeight.normal, color: Colors.black.withOpacity(0.5)),
+
+    /// labelLarge
+    labelLarge: const TextStyle().copyWith(
+        fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.black),
+
+    /// labelMedium
+    labelMedium: const TextStyle().copyWith(
+        fontSize: 14.0, fontWeight: FontWeight.bold, color: Colors.black.withOpacity(0.5)),
+  );
+
+
+
+///dark Theme
+  static TextTheme darkThemeTextTheme =  TextTheme(
+        headlineLarge: const TextStyle().copyWith(
+        fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.white),
+    headlineMedium: const TextStyle().copyWith(
+        fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.white),
+    headlineSmall: const TextStyle().copyWith(
+        fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white),
+
+    /// titleLarge
+    titleLarge: const TextStyle().copyWith(
+        fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.white),
+    titleMedium: const TextStyle().copyWith(
+        fontSize: 14.0, fontWeight: FontWeight.bold, color: Colors.white),
+    titleSmall: const TextStyle().copyWith(
+        fontSize: 12.0, fontWeight: FontWeight.bold, color: Colors.white),
+
+    /// bodyLarge
+    bodyLarge: const TextStyle().copyWith(
+        fontSize: 16.0, fontWeight: FontWeight.normal, color: Colors.white),
+    bodyMedium: const TextStyle(
+        fontSize: 14.0, fontWeight: FontWeight.normal, color: Colors.white),
+    bodySmall: const TextStyle(
+        fontSize: 12.0, fontWeight: FontWeight.normal, color: Colors.white),
+
+    /// labelLarge
+    labelLarge: const TextStyle().copyWith(
+        fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.white),
+
+    // labelMedium
+    labelMedium: const TextStyle().copyWith(
+        fontSize: 14.0, fontWeight: FontWeight.bold, color: Colors.white),
+  );
 }
