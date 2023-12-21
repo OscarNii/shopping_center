4686c5ae8e8a5856f9027c3686a55e81e9a8e3e6
immanuelsowahoscar@gmail.com <immanuelsowahoscar@gmail.com>
2023-12-21 09:17:22 +0100

    Configured dark and light theme setup with buttons, appbar, chip, etc

--- lib/utils/theme/custom_themes/text_field_theme.dart
+++ lib/utils/theme/custom_themes/text_field_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class TTextTheme {
-  TTextTheme._();
+class TTextFormFieldTheme {
+  TTextFormFieldTheme._();
 
   ///Light textForm Field
   static InputDecorationTheme lightTextFormFieldTheme = InputDecorationTheme(
@@ -11,24 +11,67 @@ class TTextTheme {
     labelStyle: const TextStyle().copyWith(fontSize: 14, color: Colors.black),
     hintStyle: const TextStyle().copyWith(fontSize: 14, color: Colors.black),
     errorStyle: const TextStyle().copyWith(fontStyle: FontStyle.normal),
-    floatingLabelStyle:
-        const TextStyle().copyWith(color: Colors.black.withOpacity(0.5)),
+
+    ///styles
+    ///
+    floatingLabelStyle: const TextStyle().copyWith(
+      color: Colors.black.withOpacity(0.8),
+    ),
     enabledBorder: OutlineInputBorder(
-      borderSide: const BorderSide(color: Colors.grey),
+      borderSide: const BorderSide(color: Colors.grey, width: 1),
       borderRadius: BorderRadius.circular(14),
     ),
     border: const OutlineInputBorder().copyWith(
       borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1, color: Colors.grey),
     ),
     focusedBorder: OutlineInputBorder(
       borderSide: const BorderSide(color: Colors.blue),
       borderRadius: BorderRadius.circular(14),
     ),
     errorBorder: OutlineInputBorder(
-        borderSide: BorderSide(color: Colors.red),
-        borderRadius: BorderRadius.circular(14)),
+      borderSide: const BorderSide(width: 1, color: Colors.red),
+      borderRadius: BorderRadius.circular(14),
+    ),
     focusedErrorBorder: OutlineInputBorder(
-        borderSide: BorderSide(color: Colors.orange),
-        borderRadius: BorderRadius.circular(14)),
+      borderSide: const BorderSide(color: Colors.orange),
+      borderRadius: BorderRadius.circular(14),
+    ),
+  );
+
+  ///dark textForm Field
+  static InputDecorationTheme darkTextFormFieldTheme = InputDecorationTheme(
+    errorMaxLines: 3,
+    prefixIconColor: Colors.grey,
+    suffixIconColor: Colors.grey,
+    labelStyle: const TextStyle().copyWith(fontSize: 14, color: Colors.white),
+    hintStyle: const TextStyle().copyWith(fontSize: 14, color: Colors.white),
+    errorStyle: const TextStyle().copyWith(fontStyle: FontStyle.normal),
+
+    ///styles
+    ///
+    floatingLabelStyle: const TextStyle().copyWith(
+      color: Colors.black.withOpacity(0.8),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderSide: const BorderSide(color: Colors.grey, width: 1),
+      borderRadius: BorderRadius.circular(14),
+    ),
+    border: const OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1, color: Colors.grey),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderSide: const BorderSide(color: Colors.white),
+      borderRadius: BorderRadius.circular(14),
+    ),
+    errorBorder: OutlineInputBorder(
+      borderSide: const BorderSide(width: 1, color: Colors.red),
+      borderRadius: BorderRadius.circular(14),
+    ),
+    focusedErrorBorder: OutlineInputBorder(
+      borderSide: const BorderSide(color: Colors.orange),
+      borderRadius: BorderRadius.circular(14),
+    ),
   );
 }
--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,27 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_center/utils/theme/custom_themes/elevated_bottom_theme.dart';
+import 'package:shopping_center/utils/theme/custom_themes/appBar_theme.dart';
+import 'package:shopping_center/utils/theme/custom_themes/checkbox_theme.dart';
+import 'package:shopping_center/utils/theme/custom_themes/chip_theme.dart';
+import 'package:shopping_center/utils/theme/custom_themes/text_field_theme.dart';
 import 'package:shopping_center/utils/theme/custom_themes/text_theme.dart';
+
+import 'custom_themes/buttom_sheet_theme.dart';
+import 'custom_themes/outline_buttom_theme.dart';
 
 class TAppTheme {
   TAppTheme._();
-  static final ThemeData lightTheme = ThemeData(
-      useMaterial3: true,
-      fontFamily: 'Poppins',
-      brightness: Brightness.light,
-      primaryColor: Colors.blue,
-      textTheme: TTextTheme.lightThemeTextTheme,
-      scaffoldBackgroundColor: Colors.white,
-      elevatedButtonTheme: const ElevatedButtonThemeData()
-      
-      );
-      
 
+  ///Light Theme
+  /////
+  static final ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    elevatedButtonTheme: const ElevatedButtonThemeData(),
+    chipTheme: TChipTheme.lightChipTheme,
+    outlinedButtonTheme: TOutlineButtonTheme.lightOutlineButtonTheme,
+    bottomSheetTheme: TBottomSheetTheme.lightBottomSheetTheme,
+    appBarTheme: TAppBarTheme.lightAppBarTheme,
+    checkboxTheme: TCheckBoxTheme.lightCheckBoxTheme,
+    inputDecorationTheme: TTextFormFieldTheme.lightTextFormFieldTheme,
+    textTheme: TTextTheme.lightThemeTextTheme,
+  );
+
+
+///Dark Theme
+////
   static final ThemeData darkTheme = ThemeData(
     useMaterial3: true,
     fontFamily: 'Poppins',
     brightness: Brightness.dark,
     primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    elevatedButtonTheme: const ElevatedButtonThemeData(),
+    chipTheme: TChipTheme.darkChipTheme,
+    outlinedButtonTheme: TOutlineButtonTheme.darkOutlineButtonTheme,
+    bottomSheetTheme: TBottomSheetTheme.darkBottomSheetTheme,
+    appBarTheme: TAppBarTheme.darkAppBarTheme,
+    checkboxTheme: TCheckBoxTheme.darkCheckBoxTheme,
+    inputDecorationTheme: TTextFormFieldTheme.darkTextFormFieldTheme,
     textTheme: TTextTheme.darkThemeTextTheme,
-    scaffoldBackgroundColor: Colors.white,
+    
   );
 }
