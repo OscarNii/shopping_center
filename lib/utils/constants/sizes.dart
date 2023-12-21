60b1bd466411a0961100eba40fb24ea1be15183b
immanuelsowahoscar@gmail.com <immanuelsowahoscar@gmail.com>
2023-12-21 17:39:55 +0100

    Updating essential utls helper functions, constants etc

--- lib/utils/constants/sizes.dart
+++ lib/utils/constants/sizes.dart
@@ -27,4 +27,54 @@ class TSizes {
   static const double buttonHeight = 14.0;
   static const double bUttonRadius = 12.0;
   static const double buttomWidth = 124.0;
+
+  //AppBar Height
+
+  static const double appBarHeight = 56.0;
+
+  ///Image Size
+  static const double imageThumbSize = 80.0;
+
+  //Default Spacing Sections
+
+  static const double defaultSpace = 24.0;
+  static const double spaceBtwnItems = 16.0;
+  static const double spaceBtwnSections = 32.0;
+
+  //Border Radius
+  static const double borderRadiusSm = 4.0;
+  static const double borderRadiusMd = 8.0;
+  static const double borderRadiusLg = 16.0;
+
+  //Divider Height
+  static const double dividerHeight = 1.0;
+
+  //Product Items Dimensions
+
+  static const double productItemHeight = 160.0;
+  static const double productItemRadius = 16.0;
+  static const double productItemSize = 120.0;
+
+  ///Input Field
+  static const double inputFieldRadius = 12.0;
+  static const double spaceBtwnInputField = 16.0;
+
+  ///Card Sizes
+
+  static const double cardRadiusLg = 16.0;
+  static const double cardRadiusMd = 12.0;
+  static const double cardRadiusSm = 10.0;
+  static const double cardRadiusXs = 6.0;
+  static const double cardElevation = 2.0;
+
+
+  //Image Carousel
+
+  static const double imageCarouselHeight = 200.0;
+
+  //Loading Indicator
+  static const double loadingIndicatorSize = 36.0;
+
+  //Grid View Spacing
+  static const double gridViewSpacing = 16.0;
 }
