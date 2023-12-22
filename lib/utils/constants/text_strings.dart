bd2d1278e471743ed23b8662c772f0e3a8970281
immanuelsowahoscar@gmail.com <immanuelsowahoscar@gmail.com>
2023-12-22 11:28:38 +0100

    functions for text class

---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,17 @@
+class TTexts {
+  TTexts._();
+
+  //OnBoarding Texts
+  static const String onboardingTitle1 = "Choose your preferred products";
+  static const String onboardingTitle2 = "Select Payment Mode";
+  static const String onboardingTitle3 = "Get your order delivered";
+
+  static const String onboardingSubTitle1 = "Welcome Ro Shopping Center";
+  static const String onboardingSubTitle2 =
+      "Choose your preferred Payment Path, Simple Transactions, Fast Delivery";
+  static const String onboardingSubTitle3 = "From our Doorstep to yours";
+
+  //Home
+  static const String homeAppTitle = "Good Day for at Shopping Center";
+  static const String homeAppSubTitle = "Mhanuel Codes";
+}
