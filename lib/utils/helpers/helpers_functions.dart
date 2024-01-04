82d5fc3dc380fde04ae19c1823645e78dae402da
immanuelsowahoscar@gmail.com <immanuelsowahoscar@gmail.com>
2024-01-04 14:13:30 +0100

    Adding color functions and also defining product specific colors

---
+++ lib/utils/helpers/helpers_functions.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class THelperFunctions {
+  static Color? getColor(String value) {
+    if (value == "green") {
+      return Colors.green;
+    } else if (value == 'Green'){
+      
+    }
+  }
+}
