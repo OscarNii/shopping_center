6e1cd32e4bf2ea6b9f07b315f599919160d06329
immanuelsowahoscar@gmail.com <immanuelsowahoscar@gmail.com>
2024-01-06 16:55:58 +0100

    Updates on screen devices and selection on lightMode and darkModes

--- lib/utils/helpers/helpers_functions.dart
+++ lib/utils/helpers/helpers_functions.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class THelperFunctions {
   static Color? getColor(String value) {
@@ -57,12 +58,32 @@ class THelperFunctions {
   static String truncateText(String text, int maxLength) {
     if (text.length > maxLength) {
       return text;
-    }else {
+    } else {
       return '$text substring(0, maxLength)...';
     }
   }
 
   static bool isDarkMode(BuildContext context) {
     return Theme.of(context).brightness == Brightness.dark;
+  }
+
+  static Size screenSize() {
+    return MediaQuery.of(Get.context!).size;
+  }
+
+  static double screenHeight() {
+    return MediaQuery.of(Get.context!).size.height;
+  }
+
+  static double screenWidth() {
+    return MediaQuery.of(Get.context!).size.width;
+  }
+  
+  static String getFormattedDate(DateTime date, {String format = 'dd mm yyyy'}){
+    return DateFormat(format).format(date);
+
+  } 
+  static List<T> removeDuplicate<T>(List<T> list){
+    return list.toSet().toList();
   }
 }
