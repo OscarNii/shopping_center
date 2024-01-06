366ccd0a32036511a25d19dfcf37d35053788377
immanuelsowahoscar@gmail.com <immanuelsowahoscar@gmail.com>
2024-01-06 16:04:43 +0100

    functions on navigator push on various screens

--- lib/utils/helpers/helpers_functions.dart
+++ lib/utils/helpers/helpers_functions.dart
@@ -46,4 +46,11 @@ class THelperFunctions {
           );
         });
   }
+
+  static void navigateToScreen(BuildContext context, Widget screen) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_) => screen),
+    );
+  }
 }
