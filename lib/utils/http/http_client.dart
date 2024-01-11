641308b54498b1c4dbd128874417953bf6660435
immanuelsowahoscar@gmail.com <immanuelsowahoscar@gmail.com>
2024-01-11 17:03:14 +0100

    Functions for http client helper for this project

--- lib/utils/http/http_client.dart
+++ lib/utils/http/http_client.dart
@@ -7,12 +7,14 @@ class THttpHelper {
   static const String _baseUrl =
       '#BASE_URL'; //Later we will replace it with our apiBaseUrl
 
+//GET request
   static Future<Map<String, dynamic>> get(String endpoint) async {
     final response = await http.get(Uri.parse('$_baseUrl/$endpoint'));
 
     return _handleResponse(response);
   }
 
+//POST request
   static Future<Map<String, dynamic>> post(
       String endpoint, dynamic data) async {
     final response = await http.post(
@@ -23,7 +25,7 @@ class THttpHelper {
     return _handleResponse(response);
   }
 
-
+//PUT request
   static Future<Map<String, dynamic>> put(String endpoint, dynamic data) async {
     final response = await http.put(
       Uri.parse('$_baseUrl/$endpoint'),
@@ -33,15 +35,15 @@ class THttpHelper {
     return _handleResponse(response);
   }
 
-
+//DELETE request
   static Future<Map<String, dynamic>> delete(String endpoint) async {
     final response = await http.delete(
       Uri.parse('$_baseUrl/$endpoint'),
     );
     return _handleResponse(response);
-  
   }
 
+//Handle response
   static Map<String, dynamic> _handleResponse(http.Response response) {
     if (response.statusCode == 200) {
       return json.decode(response.body);
@@ -49,5 +51,4 @@ class THttpHelper {
       throw Exception('Unable to load data: ${response.statusCode}');
     }
   }
-
 }
