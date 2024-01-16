475108b8034798b1968d81f797dcf360124e017a
immanuelsowahoscar@gmail.com <immanuelsowahoscar@gmail.com>
2024-01-16 13:31:51 +0100

    Updating storage util functions

---
+++ lib/utils/local_storage/storage_utl.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+
+class TStorage {
+  static final TStorage _instance = TStorage._internal();
+
+  factory TStorage() {
+    return _instance;
+  }
+
+  TStorage._internal();
+
+  final _storage = GetStorage();
+
+  //Generic Way to save data
+
+  Future<void> saveDat<T>(String key, T value) async {
+    await _storage.write(key, value);
+  }
+
+  //Generic method to read data
+  T? readData<T>(String key) {
+    return _storage.read<T>(key);
+  }
+
+  //Generic way to remove data
+  Future<void> removeData(String key) async {
+    await _storage.remove(key);
+  }
+
+  //Remove all data in storage
+  Future<void> removeAllData() async {
+    await _storage.erase();
+  }
+    
+}
