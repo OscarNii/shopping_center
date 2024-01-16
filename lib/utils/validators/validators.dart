7b2434e73f7ed6a2f6d8ff4a2849df0e0480a359
immanuelsowahoscar@gmail.com <immanuelsowahoscar@gmail.com>
2024-01-16 14:08:42 +0100

    writing functions for email and phone number validators for this project and will be used for a different soon

--- lib/utils/validators/validators.dart
+++ lib/utils/validators/validators.dart
@@ -12,4 +12,27 @@ class TValidator {
     }
     return null;
   }
+
+  static String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Password is required';
+    }
+    //check for min input length
+    if (value.length < 6) {
+      return 'Password must be at least 6 characters';
+      }
+    return null;
+  }    
+
+  static String? validatePhoneNumber(String? value){
+    if (value == null || value.isEmpty) {
+      return 'Phone number is required';
+    }
+    //assuming phone number is 10 digits US Phone Number format
+    final phoneNumberRegExp = RegExp(r'^\d{10}$');
+    if (!phoneNumberRegExp.hasMatch(value)) {
+      return 'Invalid phone number format';
+  }
+  return null;
+  }
 }
