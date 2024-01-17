8b04e6cc34b603b165a79e612df6e3ee51418a86
immanuelsowahoscar@gmail.com <immanuelsowahoscar@gmail.com>
2024-01-17 11:07:15 +0100

    Updates on the onboarding feature

--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -6,7 +6,7 @@ class TTexts {
   static const String onboardingTitle2 = "Select Payment Mode";
   static const String onboardingTitle3 = "Get your order delivered";
 
-  static const String onboardingSubTitle1 = "Welcome Ro Shopping Center";
+  static const String onboardingSubTitle1 = "Welcome To Shopping Center";
   static const String onboardingSubTitle2 =
       "Choose your preferred Payment Path, Simple Transactions, Fast Delivery";
   static const String onboardingSubTitle3 = "From our Doorstep to yours";
