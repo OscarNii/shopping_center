987af99e0d005ab11b37b151eef79eab31ae0f8d
OscarNii <immanuelsowahoscar@gmail.com>
2024-01-18 12:52:26 +0100

    Updates

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -9,15 +9,13 @@ class NavigationMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = Get.put(NavigationController());
 
-
     return Scaffold(
       bottomNavigationBar: NavigationBar(
         height: 80,
         elevation: 0,
         selectedIndex: controller.selectedIndex.value,
-        onDestinationSelected: (index) =>  controller.selectedIndex.value = index,
-
-
+        onDestinationSelected: (index) =>
+            controller.selectedIndex.value = index,
         destinations: const [
           NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
           NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
@@ -30,14 +28,13 @@ class NavigationMenu extends StatelessWidget {
   }
 }
 
-
-class NavigationController extends GetxController{
+class NavigationController extends GetxController {
   final Rx<int> selectedIndex = 0.obs;
 
   final screens = [
-    Container(color: Colors.green,),
-    Container(color: Colors.purple,),
-    Container( color: Colors.blue),
-    Container( color: Colors.red,),
+    Container(color: Colors.green),
+    Container(color: Colors.purple),
+    Container(color: Colors.blue),
+    Container(color: Colors.red),
   ];
 }
