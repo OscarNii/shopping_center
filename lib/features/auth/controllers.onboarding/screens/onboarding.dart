a854e40c78c284137666b2a66176a503504a89bf
OscarNii <immanuelsowahoscar@gmail.com>
2024-01-19 14:57:23 +0100

    [GETX] Instance "GetMaterialController" has been created

--- lib/features/auth/controllers.onboarding/screens/onboarding.dart
+++ lib/features/auth/controllers.onboarding/screens/onboarding.dart
@@ -22,7 +22,7 @@ class OnBoardingController extends GetxController {
 //hop to next page
   void nextPage() {
     if (currentPageIndex.value == 2) {
-      Get.offAll(const LoginScreen());
+      Get.offAll(() => const LoginScreen());
     } else {
       int page = currentPageIndex.value + 1;
       pageController.jumpToPage(page);
