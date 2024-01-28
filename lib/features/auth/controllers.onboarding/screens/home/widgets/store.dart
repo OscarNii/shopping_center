8676a023167e053f3fc7078350f33d42149d50e0
OscarNii <immanuelsowahoscar@gmail.com>
2024-01-28 18:44:02 +0100

    Widgets created for the navigation bottom sheets

---
+++ lib/features/auth/controllers.onboarding/screens/home/widgets/store.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class StoreScreen extends StatelessWidget {
+  const StoreScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            onPressed: () => Get.back(),
+            icon: const Icon(CupertinoIcons.clear),
+          )
+        ],
+      ),
+      body: const Center(
+        child: Text('Store'),
+      ),
+    );
+  }
+}
