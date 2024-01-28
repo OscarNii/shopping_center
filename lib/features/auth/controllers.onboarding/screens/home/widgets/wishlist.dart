5b8800355165e82f684a60cb12a5ca830a49fc2a
OscarNii <immanuelsowahoscar@gmail.com>
2024-01-28 18:42:35 +0100

    Widgets created for the navigation bottom sheets

--- lib/features/auth/controllers.onboarding/screens/home/widgets/wishlist.dart
+++ lib/features/auth/controllers.onboarding/screens/home/widgets/wishlist.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
+class WishlistScreen extends StatelessWidget {
+  const WishlistScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            onPressed: () => Get.back(),
+            icon: const Icon(CupertinoIcons.clear),
+          )
+        ],
+      ),
+      body: const Center(
+        child: Text('Wishlist'),
+      ),
+    );
+  }
+}
