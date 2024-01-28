b122995a567efcc48389491d366e5dd80458cbb1
OscarNii <immanuelsowahoscar@gmail.com>
2024-01-28 18:39:30 +0100

    Widgets created for the navigation bottom sheets

---
+++ lib/features/auth/controllers.onboarding/screens/home/widgets/profile.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            onPressed: () => Get.back(),
+            icon: const Icon(CupertinoIcons.clear),
+          )
+        ],
+      ),
+      body: const Center(
+        child: Text('Profile'),
+      ),
+    );
+  }
+}
