a379fffa4a7dd523c346613afb7f37e849b79744
OscarNii <immanuelsowahoscar@gmail.com>
2024-02-02 16:12:59 +0100

    Separate widgets created to reused for this project

--- lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
+++ lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:shopping_center/common/style/widgets/containers/tcontainer.dart';
 import 'package:shopping_center/utils/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -17,46 +18,21 @@ class HomeScreen extends StatelessWidget {
             padding: const EdgeInsets.all(0),
             child: Stack(
               children: [
-                TContainer(),
-                
+                Positioned(
+                    top: -150,
+                    right: -250,
+                    child: TContainer(
+                        backgroundColor: TColors.textWhite.withOpacity(0.1))),
+                Positioned(
+                    top: -100,
+                    right: -300,
+                    child: TContainer(
+                        backgroundColor: TColors.textWhite.withOpacity(0.1))),
               ],
             ),
           )
         ],
       ),
     ));
-  }
-}
-
-class TContainer extends StatelessWidget {
-  const TContainer({
-    super.key,
-    this.width = 400,
-    this.height = 400,
-    this.radius = 400,
-    this.padding = 0,
-    this.child,
-    this.background = TColors.light,
-  });
-
-  final double? width;
-  final double? height;
-  final double? radius;
-  final double? padding;
-  final Widget? child;
-  final Color background;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 400,
-      height: 400,
-      padding: const EdgeInsets.all(0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(400),
-        color: TColors.textWhite.withOpacity(0.1),
-      ),
-      child: child,
-    );
   }
 }
