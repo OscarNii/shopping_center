2607ee88948f22ba6a3c36af8aeb7a990f0e997b
OscarNii <immanuelsowahoscar@gmail.com>
2024-02-02 15:18:53 +0100

    updates

--- lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
+++ lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
@@ -15,7 +15,14 @@ class HomeScreen extends StatelessWidget {
             padding: const EdgeInsets.all(0),
             child: Stack(
               children: [
-                Container(),
+                Container(
+                    width: 400,
+                    height: 400,
+                    padding: const EdgeInsets.all(0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(400),
+                      color: TColors.textWhite.withOpacity(0.1),
+                    )),
               ],
             ),
           )
