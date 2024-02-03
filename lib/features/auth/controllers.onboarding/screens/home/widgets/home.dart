16b862e619961b5c99ea7427e765405ff90c64f7
OscarNii <immanuelsowahoscar@gmail.com>
2024-02-04 00:40:00 +0100

    updates

--- lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
+++ lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
@@ -13,35 +13,42 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
         body: SingleChildScrollView(
       child: Column(
-        children: [
-          ClipPath(
-            clipper: TCurvedEdges(),
-            child: Container(
-              color: TColors.primary,
-              padding: const EdgeInsets.all(0),
-              child: SizedBox(
-                height: 400,
-                child: Stack(
-                  children: [
-                    Positioned(
-                        top: -150,
-                        right: -250,
-                        child: TContainer(
-                            backgroundColor:
-                                TColors.textWhite.withOpacity(0.1))),
-                    Positioned(
-                        top: 100,
-                        right: -300,
-                        child: TContainer(
-                            backgroundColor:
-                                TColors.textWhite.withOpacity(0.1))),
-                  ],
-                ),
-              ),
-            ),
-          )
-        ],
+        children: [TContainerHeader()],
       ),
     ));
+  }
+}
+
+class TContainerHeader extends StatelessWidget {
+  const TContainerHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: TCurvedEdges(),
+      child: Container(
+        color: TColors.primary,
+        padding: const EdgeInsets.all(0),
+        child: SizedBox(
+          height: 400,
+          child: Stack(
+            children: [
+              Positioned(
+                  top: -150,
+                  right: -250,
+                  child: TContainer(
+                      backgroundColor: TColors.textWhite.withOpacity(0.1))),
+              Positioned(
+                  top: 100,
+                  right: -300,
+                  child: TContainer(
+                      backgroundColor: TColors.textWhite.withOpacity(0.1))),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
