64bb2e0ed53153ff7c1f652d415eb681e1278db9
OscarNii <immanuelsowahoscar@gmail.com>
2024-02-04 00:02:11 +0100

    clip path custom shape writing well to the codes of the app

--- lib/common/style/widgets/containers/shapes/curved_edges.dart
+++ lib/common/style/widgets/containers/shapes/curved_edges.dart
@@ -15,9 +15,15 @@ class TCurvedEdges extends CustomClipper<Path> {
     final secondLastCurve = Offset(size.width - 30, size.height - 20);
     path.quadraticBezierTo(secondFirstCurve.dx, secondFirstCurve.dy,
         secondLastCurve.dx, lastCurve.dy);
-    
-    final thirdFirstCurve = Offset(size.width, size.height -20);
-    
+
+    final thirdFirstCurve = Offset(size.width, size.height - 20);
+    final thirdLastCurve = Offset(size.width, size.height);
+    path.quadraticBezierTo(thirdFirstCurve.dx, thirdFirstCurve.dy,
+        thirdLastCurve.dx, thirdLastCurve.dy);
+
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
   }
 
   @override
--- lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
+++ lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:shopping_center/common/style/widgets/containers/shapes/curved_edges.dart';
 import 'package:shopping_center/common/style/widgets/containers/tcontainer.dart';
 import 'package:shopping_center/utils/constants/colors.dart';
 
@@ -13,24 +14,29 @@ class HomeScreen extends StatelessWidget {
         body: SingleChildScrollView(
       child: Column(
         children: [
-          Container(
-            color: TColors.primary,
-            padding: const EdgeInsets.all(0),
-            child: SizedBox(
-              height: 400,
-              child: Stack(
-                children: [
-                  Positioned(
-                      top: -150,
-                      right: -250,
-                      child: TContainer(
-                          backgroundColor: TColors.textWhite.withOpacity(0.1))),
-                  Positioned(
-                      top: 100,
-                      right: -300,
-                      child: TContainer(
-                          backgroundColor: TColors.textWhite.withOpacity(0.1))),
-                ],
+          ClipPath(
+            clipper: TCurvedEdges(),
+            child: Container(
+              color: TColors.primary,
+              padding: const EdgeInsets.all(0),
+              child: SizedBox(
+                height: 400,
+                child: Stack(
+                  children: [
+                    Positioned(
+                        top: -150,
+                        right: -250,
+                        child: TContainer(
+                            backgroundColor:
+                                TColors.textWhite.withOpacity(0.1))),
+                    Positioned(
+                        top: 100,
+                        right: -300,
+                        child: TContainer(
+                            backgroundColor:
+                                TColors.textWhite.withOpacity(0.1))),
+                  ],
+                ),
               ),
             ),
           )
