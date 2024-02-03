51da619ac1dbefa8ba72e251303a10ea0a7cb33c
OscarNii <immanuelsowahoscar@gmail.com>
2024-02-03 23:22:56 +0100

    updates

--- lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
+++ lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
@@ -16,19 +16,22 @@ class HomeScreen extends StatelessWidget {
           Container(
             color: TColors.primary,
             padding: const EdgeInsets.all(0),
-            child: Stack(
-              children: [
-                Positioned(
-                    top: -150,
-                    right: -250,
-                    child: TContainer(
-                        backgroundColor: TColors.textWhite.withOpacity(0.1))),
-                Positioned(
-                    top: -100,
-                    right: -300,
-                    child: TContainer(
-                        backgroundColor: TColors.textWhite.withOpacity(0.1))),
-              ],
+            child: SizedBox(
+              height: 400,
+              child: Stack(
+                children: [
+                  Positioned(
+                      top: -150,
+                      right: -250,
+                      child: TContainer(
+                          backgroundColor: TColors.textWhite.withOpacity(0.1))),
+                  Positioned(
+                      top: 100,
+                      right: -300,
+                      child: TContainer(
+                          backgroundColor: TColors.textWhite.withOpacity(0.1))),
+                ],
+              ),
             ),
           )
         ],
