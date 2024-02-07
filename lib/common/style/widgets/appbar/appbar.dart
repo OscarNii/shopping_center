9df6c9078dc487d443856ec982f3b8d9b802ac9d
OscarNii <immanuelsowahoscar@gmail.com>
2024-02-07 16:15:01 +0100

    updates

--- lib/common/style/widgets/appbar/appbar.dart
+++ lib/common/style/widgets/appbar/appbar.dart
@@ -2,17 +2,27 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:shopping_center/utils/constants/sizes.dart';
 import 'package:shopping_center/utils/device/device_util.dart';
 
 class TAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const TAppBar({super.key, this.title, this.leadingIcon, this.actions, this.leadingOnPress, this.showBackArrow = true,});
+  const TAppBar({
+    super.key,
+    this.title,
+    this.leadingIcon,
+    this.actions,
+    this.leadingOnPressed,
+    this.showBackArrow = true,
+  });
 
-final Widget? title;
-final Widget? leadingIcon;
-final List<Widget>? actions;
-final bool? showBackArrow;
-final VoidCallback? leadingOnPress;
+  final Widget? title;
+  final Widget? leadingIcon;
+  final List<Widget>? actions;
+  final bool? showBackArrow;
+  final VoidCallback? leadingOnPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +32,17 @@ final VoidCallback? leadingOnPress;
       ),
       child: AppBar(
         automaticallyImplyLeading: false,
-        leading: ,
+        leading: showBackArrow!
+            ? IconButton(
+                onPressed: () => Get.back(),
+                icon: const Icon(Iconsax.arrow_left))
+            : leadingIcon != null ? IconButton(
+                onPressed: leadingOnPressed,
+                icon: Icon(leadingIcon as IconData?)) : null,
+                title: title,
+        actions: actions,
+        elevation: 0,
+        
       ),
     );
   }
