40a47ed873477f33ee048fbf0d0ab229598ce9ea
OscarNii <immanuelsowahoscar@gmail.com>
2024-02-07 15:58:09 +0100

    Writing functions on the AppBar that will be used later in the appBar section

--- lib/common/style/widgets/appbar/appbar.dart
+++ lib/common/style/widgets/appbar/appbar.dart
@@ -6,7 +6,13 @@ import 'package:shopping_center/utils/constants/sizes.dart';
 import 'package:shopping_center/utils/device/device_util.dart';
 
 class TAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const TAppBar({super.key});
+  const TAppBar({super.key, this.title, this.leadingIcon, this.actions, this.leadingOnPress, this.showBackArrow = true,});
+
+final Widget? title;
+final Widget? leadingIcon;
+final List<Widget>? actions;
+final bool? showBackArrow;
+final VoidCallback? leadingOnPress;
 
   @override
   Widget build(BuildContext context) {
