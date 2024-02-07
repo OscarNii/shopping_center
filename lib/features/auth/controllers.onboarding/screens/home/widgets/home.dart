2c4a417823231385b6dd0eacb8dcc9fdaf74a55e
OscarNii <immanuelsowahoscar@gmail.com>
2024-02-07 16:26:48 +0100

    updates

--- lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
+++ lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
@@ -1,12 +1,9 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
-import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:shopping_center/common/style/widgets/appbar/appbar.dart';
-import 'package:shopping_center/common/style/widgets/containers/shapes/curved_edges.dart';
-import 'package:shopping_center/common/style/widgets/containers/tcontainer.dart';
 import 'package:shopping_center/common/style/widgets/tcontainers_header.dart';
-import 'package:shopping_center/utils/constants/colors.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
