d30220eae10827bb338e693a0838ebb0528627b4
OscarNii <immanuelsowahoscar@gmail.com>
2024-02-13 15:58:22 +0100

    Writing seperate container to stack all the shapes that i will be writing in them

---
+++ lib/common/style/widgets/containers/circular_container.dart
@@ -0,0 +1,36 @@
+
+import 'package:flutter/material.dart';
+import 'package:shopping_center/utils/constants/colors.dart';
+
+class TCircularContainer extends StatelessWidget {
+  const TCircularContainer({
+    super.key,
+    this.child,
+    this.width = 400,
+    this.height = 400,
+    this.padding = 0,
+    this.radius = 400,
+    this.backgroundColor = TColors.primary,
+  });
+
+  final double? width;
+  final double? height;
+  final double padding;
+  final double radius;
+  final Color backgroundColor;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      padding: EdgeInsets.all(0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(400),
+        color: TColors.textWhite.withOpacity(0.1),
+      ),
+      child: child,
+    );
+  }
+}
--- lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
+++ lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:shopping_center/common/style/widgets/containers/circular_container.dart';
 import 'package:shopping_center/utils/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -17,46 +18,15 @@ class HomeScreen extends StatelessWidget {
               padding: EdgeInsets.all(0),
               child: Stack(
                 children: [
-                  TCircularContainer(),
+                  TCircularContainer(
+                    backgroundColor: TColors.textWhite.withOpacity(0.1),
+                  ),
                 ],
               ),
             )
           ],
         ),
       ),
-    );
-  }
-}
-
-class TCircularContainer extends StatelessWidget {
-  const TCircularContainer({
-    super.key,
-    this.child,
-    this.width = 400,
-    this.height = 400,
-    this.padding = 0,
-    this.radius = 400,
-    this.backgroundColor = TColors.primary,
-  });
-
-  final double? width;
-  final double? height;
-  final double padding;
-  final double radius;
-  final Color backgroundColor;
-  final Widget? child;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 400,
-      height: 400,
-      padding: EdgeInsets.all(0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(400),
-        color: TColors.textWhite.withOpacity(0.1),
-      ),
-      child: child,
     );
   }
 }
