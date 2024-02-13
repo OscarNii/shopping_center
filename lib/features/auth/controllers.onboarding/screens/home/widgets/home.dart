4f1814d1859565ea8372f921ab6198aa0375dd70
OscarNii <immanuelsowahoscar@gmail.com>
2024-02-13 16:04:01 +0100

    created a double circular container withOpacity of 0 on both sides of the AppBar

--- lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
+++ lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
@@ -16,12 +16,27 @@ class HomeScreen extends StatelessWidget {
             Container(
               color: TColors.primary,
               padding: EdgeInsets.all(0),
-              child: Stack(
-                children: [
-                  TCircularContainer(
-                    backgroundColor: TColors.textWhite.withOpacity(0.1),
-                  ),
-                ],
+              child: SizedBox(
+                height: 400,
+                width: double.infinity,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: -150,
+                      right: -250,
+                      child: TCircularContainer(
+                        backgroundColor: TColors.textWhite.withOpacity(0.1),
+                      ),
+                    ),
+                    Positioned(
+                      top: 100,
+                      right: -300,
+                      child: TCircularContainer(
+                        backgroundColor: TColors.textWhite.withOpacity(0.1),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             )
           ],
