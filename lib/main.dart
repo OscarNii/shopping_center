8831af70310baedcac75f30e8bd655c09c39e926
kwaw kumi miezah <k.kumimiezah@gmail.com>
2024-02-14 10:11:04 +0000

    supabase-init

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://lpjfmqijznavmhyosqwm.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxwamZtcWlqem5hdm1oeW9zcXdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5MDM4MjIsImV4cCI6MjAyMzQ3OTgyMn0.kO65rpnK2eySz-Ym5iBGeHNsGIyh8oWxqYDxsE6gBJ8',
+    );
   runApp(
     const App(),
   );
