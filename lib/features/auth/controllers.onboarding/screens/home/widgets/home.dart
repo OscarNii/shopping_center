7a99ecef912801909de257df7a682bc6cf7e7ba4
OscarNii <immanuelsowahoscar@gmail.com>
2024-02-25 17:32:18 +0100

    One part of the carousel slides is about to go down Enough for the day
    SLEEP!!!

--- lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
+++ lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
@@ -3,6 +3,9 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_center/common/style/widgets/appbar/appbar.dart';
 import 'package:shopping_center/common/style/widgets/containers/tPrimaryheader.dart';
+import 'package:shopping_center/utils/constants/colors.dart';
+import 'package:shopping_center/utils/constants/image_strings.dart';
+import 'package:shopping_center/utils/constants/sizes.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,14 +13,31 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: TPrimaryHeader(
-          child: Column(
-            children: [
-              TAppBar()
-            ],
+      body: Column(
+        children: [
+          SingleChildScrollView(
+            child: TPrimaryHeader(
+              child: Column(
+                children: [TAppBar()],
+              ),
+            ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.all(TSizes.defaultSpace),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(TSizes.md),
+                  border: Border.all(color: TColors.grey)),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(TSizes.md),
+                child: const Image(
+                  image: AssetImage(TImages.carousel4),
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
