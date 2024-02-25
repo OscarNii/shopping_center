17c57f8a3bafc4c679c52bfd64a5cf6fbcb210e6
OscarNii <immanuelsowahoscar@gmail.com>
2024-02-25 16:05:52 +0100

    various pubs and imgs on assets added under the constructing of the banner for the carousel sliders

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -22,4 +22,13 @@ class TImages {
   ///VERIFY EMAIL
   static const String verifyImg = "assets/verify.gif";
   static const String successmail = "assets/successmail.gif";
+
+  //Carousel 
+  static const String carousel1 = "assets/ASUS-OLED.jpg"; 
+  static const String carousel2 = "assets/carton.jpg";
+  static const String carousel3 = "assets/fruit.jpg";
+  static const String carousel4 = "assets/laptop.png";
+  static const String carousel5 = "assets/luVi.png";
+  static const String carousel6 = "assets/nike-11.jpg";
+
 }
