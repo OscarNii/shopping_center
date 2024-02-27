4533f3095176c59381040eb9d1b9121209f36583
kwaw kumi miezah <k.kumimiezah@gmail.com>
2024-02-27 14:08:24 +0000

    issues resolved

--- lib/features/auth/controllers.onboarding/screens/sign up/sign_up.dart
+++ lib/features/auth/controllers.onboarding/screens/sign up/sign_up.dart
@@ -227,7 +227,7 @@ class SignUP extends StatelessWidget {
                         )
               ],
             ),
-          )),
-        ));
+          )]),
+        ))));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ Future<void> main() async {
   await Supabase.initialize(
     url: "https://lpjfmqijznavmhyosqwm.supabase.co",
     anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxwamZtcWlqem5hdm1oeW9zcXdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5MDM4MjIsImV4cCI6MjAyMzQ3OTgyMn0.kO65rpnK2eySz-Ym5iBGeHNsGIyh8oWxqYDxsE6gBJ8",    
-    )
+    );
   runApp(
     const App(),
   );
