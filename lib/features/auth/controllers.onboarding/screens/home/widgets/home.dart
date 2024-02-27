aa72cdcb1547e6f8f7da4f238944785270891703
OscarNii <immanuelsowahoscar@gmail.com>
2024-02-27 12:26:35 +0100

    Building carousel slides

--- lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
+++ lib/features/auth/controllers.onboarding/screens/home/widgets/home.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:shopping_center/banners/bannersImages.dart';
 import 'package:shopping_center/common/style/widgets/appbar/appbar.dart';
@@ -23,9 +24,18 @@ class HomeScreen extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(TSizes.defaultSpace),
-            child: TBanner1(imageUrl: TImages.carousel3),
-          )
+              padding: const EdgeInsets.all(TSizes.defaultSpace),
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  viewportFraction: 1,
+                ),
+                items: const [
+                  TBanner1(imageUrl: TImages.carousel3),
+                  TBanner1(imageUrl: TImages.carousel4),
+                  TBanner1(imageUrl: TImages.carousel5),
+                  TBanner1(imageUrl: TImages.carousel1),
+                ],
+              ))
         ],
       ),
     );
