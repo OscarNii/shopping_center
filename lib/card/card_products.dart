60cbceb684d948719d2186e2a37356454a87ea9a
OscarNii <immanuelsowahoscar@gmail.com>
2024-02-29 13:30:24 +0100

    Commented the widget, we'll continue tomorrow

--- lib/card/card_products.dart
+++ lib/card/card_products.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_center/banners/bannersImages.dart';
 import 'package:shopping_center/common/shadow/shadow_style.dart';
 import 'package:shopping_center/round_container/round_Containers.dart';
 import 'package:shopping_center/utils/constants/colors.dart';
+import 'package:shopping_center/utils/constants/image_strings.dart';
 import 'package:shopping_center/utils/constants/sizes.dart';
 import 'package:shopping_center/utils/helpers/helpers_functions.dart';
 
@@ -25,10 +27,21 @@ class TCardsProducts extends StatelessWidget {
             height: 180,
             padding: const EdgeInsets.all(TSizes.sm),
             backgroundColor: dark ? TColors.darkGrey : TColors.light,
-            child: const Stack(
+            child: Stack(
               children: [
-                
-                
+                const TBanner1(
+                    imageUrl: TImages.productImage1, applyImageRadius: true),
+                TRcontainer(
+                  radius: TSizes.sm,
+                  backgroundColor: TColors.secondary.withOpacity(0.8),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: TSizes.sm, vertical: TSizes.xs),
+                  child: Text('30%',
+                      style: Theme.of(context)
+                          .textTheme
+                          .labelLarge!
+                          .apply(color: TColors.dark)),
+                )
               ],
             ),
           )
--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -1,4 +1,4 @@
- // ignore_for_file: unnecessary_string_escapes
+// ignore_for_file: unnecessary_string_escapes
 
 class TImages {
   TImages._();
@@ -12,7 +12,6 @@ class TImages {
   static const String googleLogo = 'assets/google.png';
   static const String facebookLogo = 'assets/facebook.png';
   static const String githubLogo = 'assets/github.png';
-  
 
   ///Onboarding Images
   static const String onboardingImage1 = "assets/shop.png";
@@ -23,12 +22,13 @@ class TImages {
   static const String verifyImg = "assets/verify.gif";
   static const String successmail = "assets/successmail.gif";
 
-  //Carousel 
-  static const String carousel1 = "assets/ASUS-OLED.jpg"; 
+  //Carousel
+  static const String carousel1 = "assets/ASUS-OLED.jpg";
   static const String carousel2 = "assets/carton.jpg";
   static const String carousel3 = "assets/fruit.jpg";
   static const String carousel4 = "assets/laptop.png";
   static const String carousel5 = "assets/luVi.png";
   static const String carousel6 = "assets/nike-11.jpg";
-
+  static const String productImage1 =
+      "assets/Athlete-Football-Boots-PNG-Image.png";
 }
